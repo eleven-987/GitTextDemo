c4d6454671ca1e63066a863ab3034b215c6e880f
wuwenye <wwwlzy987@163.com>
2020-12-10 15:32:35 +0800

    第一次提交

--- app/src/main/java/com/example/gittextdemo/MainActivity.java
+++ app/src/main/java/com/example/gittextdemo/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String test = "第一次上传的大帅比";
     }
 }
